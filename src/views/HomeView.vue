5399d0613346035db88858bf43b305109dc03e56
bator <batr.fly@mail.ru>
2022-11-11 21:27:13 +0400

    add data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,80 @@
 <template>
-	<l-map style="height: 80vh" :zoom="zoom" :center="center" ref="mapRef">
-		<l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
-		<l-geo-json v-if="isShowMap" :geojson="geojson" :options="geojsonOptions" />
-	</l-map>
+	<div class="industrial-blocks">
+		<l-map class="industrial-blocks__map" style="height: 80vh" :zoom="zoom" :center="center" ref="mapRef"
+			@ready="leafletMapApi()">
+			<l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
+			<template v-for="layout in layoutsData" :key="layout.id">
+				<l-geo-json :visible="layout.isEnabled" :geojson="layout.geojson" :options="layout.options" />
+			</template>
+		</l-map>
+		<div class="industrial-blocks__info">
+			<ol v-if="isFetched">
+				<li v-for="layout in layoutsData" :key="layout.id + '_info'">
+					<label>
+						<span>{{ layout.id }}</span>
+						<input v-model="layout.isEnabled" type="checkbox">
+					</label>
+					{{ layout.isReady }}
+				</li>
+			</ol>
+			<div v-else>Загрузка ...</div>
+		</div>
+	</div>
 </template>
 <script>
-import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
-import { onMounted, ref, watch, computed } from "vue";
+import { L } from 'leaflet';
+import { LMap, LTileLayer, LGeoJson, LControl } from "@vue-leaflet/vue-leaflet";
+import { onMounted, ref, watch } from "vue";
 import { fetchGeoserverData } from "@/scripts/fetchGeoserverData";
+import { DrawAreaSelection } from '@bopen/leaflet-area-selection';
 
 export default {
-	components: { LMap, LTileLayer, LGeoJson },
+	components: { LMap, LTileLayer, LGeoJson, LControl },
 	setup() {
 		const mapRef = ref(null);
-		const geojson = ref(null);
-		const isShowMap = ref(false);
-		watch(mapRef, () => { });
-		const geojsonOptions = computed(() => ({
-			style(feature) {
-				const total = feature.properties.percent_total;
-				const red = (total / 70) * 255;
-				const green = 255 - red;
-				const color = `rgba(${red},${green},0,0.7)`;
-				return {
-					color,
-				};
-			},
-			onEachFeature(feature, layer) {
-				layer.bindPopup(
-					`total points: ${feature.properties.percent_total}`
-				);
+		const map = ref(null)
+		const layoutsData = ref(null);
+		const showLandLayout = ref(false);
+		const selectedAreaGeoJSON = ref(null);
+		const isFetched = ref(false);
+
+		const areaSelection = new DrawAreaSelection({
+			fadeOnActivation: true,
+			onPolygonReady: (polygon) => {
+				selectedAreaGeoJSON.value = polygon.toGeoJSON(3);
 			}
-		}))
-		onMounted(async () => {
-			geojson.value = await fetchGeoserverData()
-			isShowMap.value = true
+		});
+		const leafletMapApi = () => {
+			map.value = mapRef.value.leafletObject
+		}
+		watch(map, () => {
+			map.value.addControl(areaSelection)
 		})
+		watch(selectedAreaGeoJSON, () => {
+			// получать данные геосервера в пределах выделеннной области
+			console.log(selectedAreaGeoJSON.value)
+		})
+		onMounted(async () => {
+			layoutsData.value = await fetchGeoserverData()
+			isFetched.value = true;
+		})
+
 		return {
 			zoom: 11,
 			center: [55.8, 37.6],
-			geojson,
-			geojsonOptions,
-			isShowMap
+			layoutsData,
+			showLandLayout,
+			mapRef,
+			leafletMapApi,
+			isFetched
 		};
 	},
 };
 </script>
+<style>
+.industrial-blocks {
+	display: grid;
+	gap: 20px;
+	grid-template-columns: 3fr 1fr;
+}
+</style>
