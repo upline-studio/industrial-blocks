bf751ac9e901fb8a2a4c910db7e8038f68fbc934
vaiil <yxllxy@ya.ru>
2022-11-11 21:13:40 +0400

    fix: use correct link

--- src/scripts/fetchGeoserverData.js
+++ src/scripts/fetchGeoserverData.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function fetchGeoserverData() {
-	try {
-		const resp = await axios({
-			method: 'get',
-			url: `http://localhost:3080/geoserver/moscow/wms?service=WMS&version=1.1.0&request=GetMap&layers=moscow%3Aland&bbox=37.334346771240234%2C55.75446701049805%2C37.83428955078125%2C55.957977294921875&width=768&height=330&srs=EPSG%3A4326&styles=&format=geojson`,
-		});
-		return resp.data;
-	} catch (e) {
-		return e.response;
-	}
+  try {
+    const resp = await axios({
+      method: "get",
+      url: `http://37.18.110.72:8080/geoserver/moscow/wms?service=WMS&version=1.1.0&request=GetMap&layers=moscow%3Aland&bbox=37.334346771240234%2C55.75446701049805%2C37.83428955078125%2C55.957977294921875&width=768&height=330&srs=EPSG%3A4326&styles=&format=geojson`,
+    });
+    return resp.data;
+  } catch (e) {
+    return e.response;
+  }
 }
